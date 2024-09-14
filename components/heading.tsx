a1e62a387c80f9632ea36e5316bb4e640b83c005
Huzaifa <huzaifanaeem909@gmail.com>
2024-09-14 21:38:44 +0500

    Update Heading

--- components/heading.tsx
+++ components/heading.tsx
@@ -7,11 +7,19 @@ export function Heading({
     text: string;
     className?: string;
 }) {
+    // Split the text into an array of words
+    const words = text.split(" ");
+    // Get all words except the last two
+    const mainText = words.slice(0, -2).join(" ");
+    // Get the last two words
+    const lastTwoWords = words.slice(-2).join(" ");
+
     return (
         <h2
-            className={cn("text-center font-bold text-3xl text-blue-100 relative z-20", className)}
+            className={cn("text-center font-bold text-4xl text-blue-100 relative z-20 mb-5", className)}
         >
-            {text}
+            {mainText}{" "}
+            <span className="text-purple">{lastTwoWords}</span>
         </h2>
     );
 }
